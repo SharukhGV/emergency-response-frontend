2f8e8da4e0ac12dbfdee123fe93b86775b6ecbda
SharukhGV <sharukhvelupillai@pursuit.org>
2023-10-23 23:35:13 -0400

    emergency categories added

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,17 +1,53 @@
 import GoogleMaps from "../components/GoogleMaps";
 
-
-
-export default function Index(){
-
-
-return(
+export default function Index() {
+  return (
     <div>
-<GoogleMaps/>
+      <GoogleMaps />
 
-</div>
-)
-
-
-
+      <div className="container">
+        <div className="item">
+          <h3>Natural Disasters</h3>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+        </div>
+        <div className="item">
+          <h3>Medical Emergencies</h3>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+        </div>
+        <div className="item">
+          <h3> Fire and Structure Emergencies</h3>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+        </div>
+        <div className="item">
+          <h3>Vehicle Emergencies</h3>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+        </div>
+        <div className="item">
+          <h3>Security and Civil Unrest</h3>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+          <p>Person Emergency Here</p>
+        </div>
+      </div>
+    </div>
+  );
 }
