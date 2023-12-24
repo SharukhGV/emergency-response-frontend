fa4d52289c1f02e3bac0976774e36210229e332e
Sharukh Velupillai <sharukh.g.velupillai@gmail.com>
2023-12-24 00:36:57 -0500

    star brightness styling page

--- src/pages/SkyBrightness.jsx
+++ src/pages/SkyBrightness.jsx
@@ -12,29 +12,29 @@ function SkyBrightness(){
 
 
 return(
-<div>
-    <div>
+<div className="skydescription">
+    <div className="skydescription">
         Here are five descriptions of sky brightness affected by light pollution, categorized by a percent, along with approximate descriptions of how many stars you might see under total darkness:
 </div>
-<strong>0-10% Light Pollution:</strong>
+<strong style={{backgroundColor:"black", color:"white"}} className="skydescription">0-10% Light Pollution:</strong>
 
-<div>Sky Description: In areas with minimal light pollution, the sky appears exceptionally dark. The Milky Way is vividly visible, and thousands of stars are discernible.
+<div className="skydescription">Sky Description: In areas with minimal light pollution, the sky appears exceptionally dark. The Milky Way is vividly visible, and thousands of stars are discernible.
 Approximate Star Visibility: Around 2,500 to 3,000 stars visible to the naked eye.</div>
-<strong>10-30% Light Pollution:</strong>
+<strong style={{backgroundColor:"black", color:"white"}} className="skydescription">10-30% Light Pollution:</strong>
 
-<div>Sky Description: Light pollution begins to affect the visibility of fainter stars, causing a visible glow near the horizon. However, major constellations and the Milky Way's core are still relatively clear.
+<div className="skydescription">Sky Description: Light pollution begins to affect the visibility of fainter stars, causing a visible glow near the horizon. However, major constellations and the Milky Way's core are still relatively clear.
 Approximate Star Visibility: Roughly 1,000 to 2,000 stars visible to the naked eye.</div>
-<strong>30-50% Light Pollution:</strong>
+<strong className="skydescription" style={{backgroundColor:"rgba(58, 55, 50, 100)", color:"white"}} >30-50% Light Pollution:</strong>
 
-<div>Sky Description: Moderate light pollution dims the view of the Milky Way and fainter stars. Only the brightest stars and major constellations are easily identifiable.
+<div className="skydescription">Sky Description: Moderate light pollution dims the view of the Milky Way and fainter stars. Only the brightest stars and major constellations are easily identifiable.
 Approximate Star Visibility: Around 500 to 1,000 stars visible to the naked eye.</div>
-<strong>50-70% Light Pollution:</strong>
+<strong style={{backgroundColor:"rgb(85, 72, 43)", color:"white"}} className="skydescription">50-70% Light Pollution:</strong>
 
-<div>Sky Description: Substantial light pollution obscures most of the Milky Way and dimmer stars, leaving only the brightest stars and planets visible. The sky appears significantly brightened, even at night.
+<div className="skydescription">Sky Description: Substantial light pollution obscures most of the Milky Way and dimmer stars, leaving only the brightest stars and planets visible. The sky appears significantly brightened, even at night.
 Approximate Star Visibility: Approximately 100 to 500 stars visible to the naked eye.</div>
-<strong>70-100% Light Pollution:</strong>
+<strong style={{backgroundColor:"rgb(119, 85, 10)", color:"white"}} className="skydescription">70-100% Light Pollution:</strong>
 
-<div>Sky Description: Heavily affected by light pollution, the sky is illuminated, making it challenging to distinguish individual stars or constellations. Only the very brightest celestial objects, such as the Moon, planets, and a few prominent stars, are visible.
+<div className="skydescription">Sky Description: Heavily affected by light pollution, the sky is illuminated, making it challenging to distinguish individual stars or constellations. Only the very brightest celestial objects, such as the Moon, planets, and a few prominent stars, are visible.
 Approximate Star Visibility: Less than 100 stars visible to the naked eye, often limited to the brightest stars and planets.</div>
 
 <p>Please note that these descriptions and star visibility counts can vary based on factors such as atmospheric conditions, time of year, and specific locations within each light pollution category.</p>
