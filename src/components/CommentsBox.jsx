2694f6d6492eb221451efa822ec31751bba0a651
Sharukh Velupillai <sharukh.g.velupillai@gmail.com>
2024-01-20 23:46:19 -0500

    added profile pic to comments ind.

--- src/components/CommentsBox.jsx
+++ src/components/CommentsBox.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import { useState,useEffect } from "react";
 function CommentsBox({ commentz, uuid, loginUsername, id, index }) {
     const deleteComment = (identification) => {
         axios
@@ -14,6 +14,17 @@ function deleteCommentNow(){
 }
 
 
+const [dataProfile, setDataProfile]=useState([])
+    useEffect(() => {
+        axios
+          .get(`${import.meta.env.VITE_BACKEND_API}/profile`)
+          .then((response) => setDataProfile(response.data))
+          .catch((e) => console.error("catch", e));
+      }, []);
+
+      const matchingProfile = dataProfile.find(prof => prof.my_username===commentz.my_username);
+
+
     function parseDATE(date) {
         return `${date.charAt(5)}${date.charAt(6)} / ${date.charAt(8)}${date.charAt(9)} / ${date.charAt(0)}${date.charAt(1)}${date.charAt(2)}${date.charAt(3)}`
       }
@@ -21,6 +32,7 @@ function deleteCommentNow(){
       <div style={{backgroundColor:"black", color:"yellow",border:"solid", padding:"30px", margin:"10px", borderRadius:"30px"}} key={uuid}>
         {/* {parseFloat(commentz.findspot_id) === parseFloat(id) ? ( */}
             <div>
+               {matchingProfile ?<img style={{width:"50px", height:"50px", borderRadius:"50%"}} src={matchingProfile.image_url}></img> :null}
                 <div>{parseDATE(commentz.date)}</div>
                 <div style={{fontSize:"10px"}}>{commentz.my_username}</div>
           <div style={{fontSize:"15px"}}>{commentz.description}</div></div>
