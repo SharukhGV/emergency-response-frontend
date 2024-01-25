7dfe51b6cd58084809392bbb3cdfc6b451097e17
Sharukh Velupillai <sharukh.g.velupillai@gmail.com>
2024-01-24 23:46:37 -0500

    added meteor shower storm leonid 1833 vid and 101 lesson

--- src/pages/MeteorShowers.jsx
+++ src/pages/MeteorShowers.jsx
@@ -8,9 +8,10 @@ console.log(meteorShowers.meteorShowers)
     return (
 <>
 
+<h1>Meteor Showers</h1>
+{/* <img src={meteorpics} style={{width:"350px"}}></img> */}
+<iframe width="560" height="315" src="https://www.youtube.com/embed/teKEkLNoYTk?si=zazlj1jeqN3Egojg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
-<h1 style={{color:"black"}}>Meteor Showers</h1>
-<img src={meteorpics} style={{width:"350px"}}></img>
         <div>{meteorShowers.meteorShowers.map(meteor => {
             return (
 <div key={meteor.name} style={{ color:"green", boarderRadius:"7%", padding:"3px"}}>
@@ -21,7 +22,8 @@ console.log(meteorShowers.meteorShowers)
             )
         })
         }</div>
-        
+<iframe width="560" height="315" src="https://www.youtube.com/embed/zkglsg0K1IY?si=S_ivIm88rzbQqmjR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
      <br></br>   {/* </div> */}
         <br></br>
 </>
