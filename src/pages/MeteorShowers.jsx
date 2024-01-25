5671a1b827326e2b8fc2969a50263ce4cc921569
Sharukh Velupillai <sharukh.g.velupillai@gmail.com>
2024-01-25 06:06:29 -0500

    meteor shower page redesigning

--- src/pages/MeteorShowers.jsx
+++ src/pages/MeteorShowers.jsx
@@ -1,6 +1,8 @@
 
 import meteorShowers from "./meteorShower.json"
 import meteorpics from "./meteorpics.png"
+
+import "./meteorshowers.css"
 function MeteorShowers() {
 
 console.log(meteorShowers.meteorShowers)
@@ -8,21 +10,27 @@ console.log(meteorShowers.meteorShowers)
     return (
 <>
 
-<h1>Meteor Showers</h1>
+<div class="homecontainerMeteors">
+  <div class="youtube"><iframe width="100%" height="50%" src="https://www.youtube.com/embed/teKEkLNoYTk?si=zazlj1jeqN3Egojg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  <iframe width="100%" height="50%" src="https://www.youtube.com/embed/zkglsg0K1IY?si=S_ivIm88rzbQqmjR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+</div>
+  <div class="meteorInfo"><h1>Meteor Showers</h1>
 {/* <img src={meteorpics} style={{width:"350px"}}></img> */}
-<iframe width="auto" height="auto" src="https://www.youtube.com/embed/teKEkLNoYTk?si=zazlj1jeqN3Egojg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
         <div>{meteorShowers.meteorShowers.map(meteor => {
             return (
-<div key={meteor.name} style={{ color:"green", boarderRadius:"7%", padding:"3px"}}>
-                <h2 style={{color:"gray"}}> {meteor.name}</h2>
-                <h3>{meteor.date}</h3>
-                <h4>{meteor.frequency}</h4>
+<div key={meteor.name} style={{ color:"orange", boarderRadius:"7%", padding:"3px"}}>
+                <h2 style={{color:"yellow"}}> {meteor.name}</h2>
+                <h3  style={{color:"gray"}}>{meteor.date}</h3>
+                <h4  style={{color:"red"}}>{meteor.frequency}</h4>
                 </div>
             )
         })
-        }</div>
-<iframe width="auto" height="auto" src="https://www.youtube.com/embed/zkglsg0K1IY?si=S_ivIm88rzbQqmjR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        }</div></div>
+</div>
+
+
 
      <br></br>   {/* </div> */}
         <br></br>
