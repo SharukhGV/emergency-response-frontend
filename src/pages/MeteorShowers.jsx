eeb4f9a935186a72b83e16dc0e9f09d21d33d16f
Sharukh Velupillai <sharukh.g.velupillai@gmail.com>
2024-01-25 22:15:52 -0500

    even odd div meteorshower background color

--- src/pages/MeteorShowers.jsx
+++ src/pages/MeteorShowers.jsx
@@ -15,12 +15,12 @@ console.log(meteorShowers.meteorShowers)
   <iframe width="100%" height="50%" src="https://www.youtube.com/embed/zkglsg0K1IY?si=S_ivIm88rzbQqmjR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
 </div>
-  <div class="meteorInfo"><h1>Meteor Showers</h1>
+  <div  class="meteorInfo"><h1 style={{background:"linear-gradient(90deg, rgba(255,248,0,0.3169642857142857) 36%, rgba(0,112,255,0.5942752100840336) 49%, rgba(255,0,0,0.4990371148459384) 63%)"}}>Meteor Showers</h1>
 {/* <img src={meteorpics} style={{width:"350px"}}></img> */}
 
-        <div>{meteorShowers.meteorShowers.map(meteor => {
+        <div >{meteorShowers.meteorShowers.map(meteor => {
             return (
-<div key={meteor.name} style={{ color:"orange", boarderRadius:"7%", padding:"3px"}}>
+<div key={meteor.name} className="meteorMap" style={{ color:"orange", boarderRadius:"7%", padding:"3px"}}>
                 <h2 style={{color:"yellow"}}> {meteor.name}</h2>
                 <h3  style={{color:"gray"}}>{meteor.date}</h3>
                 <h4  style={{color:"red"}}>{meteor.frequency}</h4>
