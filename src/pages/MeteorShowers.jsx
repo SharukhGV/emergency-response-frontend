e26b2059c2221fdcfaa1e4468080dc0faf0bd6a8
Sharukh Velupillai <sharukh.g.velupillai@gmail.com>
2024-01-28 04:57:21 -0500

    removed youtube vids and replaced with textual info on 1833 meteor storm

--- src/pages/MeteorShowers.jsx
+++ src/pages/MeteorShowers.jsx
@@ -17,13 +17,55 @@ console.log(meteorShowers.meteorShowers)
 <>
 
 <div class="homecontainerMeteors">
-  <div class="youtube"><iframe width="100%" height="50%" src="https://www.youtube.com/embed/teKEkLNoYTk?si=zazlj1jeqN3Egojg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-  <iframe width="100%" height="50%" src="https://www.youtube.com/embed/zkglsg0K1IY?si=S_ivIm88rzbQqmjR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  <div class="youtube">
+
+  {/* <div class="card">
+  <img src={"https://upload.wikimedia.org/wikipedia/commons/f/fc/Leonids-1833.jpg"} alt="Avatar" style="width:100%"></img>
+  <div class="container"></div>
+    <h4><b>John Doe</b></h4>
+    <p>Architect & Engineer</p>
+  </div> */}
+
+
+
+{/* <div class="card">
+  <img src={"https://upload.wikimedia.org/wikipedia/commons/f/fc/Leonids-1833.jpg"} alt="Avatar" style="width:100%"></img>
+  <div class="container"></div>
+    <h4><b>John Doe</b></h4>
+    <p>Architect & Engineer</p>
+  </div> */}
+<br></br>
+
+<img style={{height:"350px", width:"auto",borderRadius:"5px"}} src={"https://upload.wikimedia.org/wikipedia/commons/f/fc/Leonids-1833.jpg"} alt="Avatar" ></img>
+    <h4><b>1833 Meteor Storm</b></h4>
+    <p>Image Credit: Bible Readings for the Home Circle</p>
+
+
+
+
+    {/* <img style={{width:"300px", height:"auto"}} src={"https://upload.wikimedia.org/wikipedia/commons/f/fc/Leonids-1833.jpg"}></img>  <img style={{width:"300px", height:"auto"}} src={"https://upload.wikimedia.org/wikipedia/commons/d/da/Leonids-Pickering.jpg"}></img> */}
+<div style={{margin:"50px"}}>
+
+<div>The Leonid meteor storm of 1833 was one of the most spectacular meteor showers in recorded history. It occurred during the early morning hours of November 13, 1833, and was particularly intense over North America. The event was characterized by an incredibly high number of meteors, with estimates ranging from tens of thousands to over 100,000 meteors per hour at its peak.</div>
+<br></br>
+<div>The meteors appeared to radiate from the constellation Leo, hence the name "Leonid." Observers reported that the sky was filled with streaks of light as if stars were falling or raining down. The meteors were so numerous that they resembled a storm rather than a typical meteor shower.</div>
+<br></br>
+<div>The 1833 Leonid meteor storm is often considered a benchmark event in the study of meteor showers. It played a crucial role in the development of our understanding of these celestial phenomena and contributed to the realization that meteor showers are associated with the debris left behind by comets.</div>
+<br></br>
+<div>The source of the 1833 Leonid meteor storm was later identified as the comet Tempel-Tuttle, which orbits the Sun and periodically sheds debris. When the Earth passes through this debris trail, the particles burn up in the atmosphere, creating the visible streaks of light known as meteors. The Leonid meteor shower occurs annually when the Earth intersects the orbit of Comet Tempel-Tuttle, but the intensity of the storm observed in 1833 was exceptional and has not been matched in recent history.</div>
+<br></br>
+<div>Subsequent Leonid meteor showers have occurred with varying intensity, with periodic outbursts when the Earth encounters denser portions of the comet's debris trail. However, the 1833 event remains a legendary and unique occurrence in the history of meteor showers.</div>
+
 
 </div>
-  <div  class="meteorInfo"><h1>Meteor Showers</h1>
-{/* <img src={meteorpics} style={{width:"350px"}}></img> */}
 
+
+</div>
+  <div  class="meteorInfo">
+    <br></br>
+    <h1>Annual Meteor Showers</h1>
+{/* <img src={meteorpics} style={{width:"350px"}}></img> */}
+<br></br>
         <div >{meteorShowers.meteorShowers.map(meteor => {
             return (
 
