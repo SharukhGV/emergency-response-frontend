bd9915a79cefe052b791c15eb8d73f26034ba1c0
Sharukh Velupillai <sharukh.g.velupillai@gmail.com>
2024-01-28 16:13:54 -0500

    added bee hive background to about section on homepage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -28,10 +28,10 @@ return(
 {/* <img style={{width:"300px"}} src={community}></img> */}
 {/* <div style={{backgroundColor:"#FFFDDF", padding:"30px", borderRadius:"20px"}}> */}
 {/* <h4 style={{margin:"50px"}}>Embark on a celestial journey with our community-powered app, a mesmerizing portal where the cosmos converges with human wonder. Within this celestial haven, users weave tales of starry night destinations, crafting geotagged posts that illuminate the secrets of the nocturnal sky. </h4> */}
-<h5 style={{margin:"50px"}}>A Bee Hive is full of... well... bees! The night sky is full of stars.
+<h5 style={{margin:"50px",backgroundColor:"rgba(0,0,0,0.5)", borderRadius:"5px",padding:"10px"}}>A Bee Hive is full of... well... bees! The night sky is full of stars.
 This app is meant to be a culmination of useful information for the
 average city-dweller to find and get to a place of starry beauty.</h5>
-<h5 style={{margin:"50px"}}>Almost 80% of the world's populations cannot see the Milky Way band, and
+<h5 style={{margin:"50px",backgroundColor:"rgba(0,0,0,0.5)", borderRadius:"5px"}}>Almost 80% of the world's populations cannot see the Milky Way band, and
 such beauty being hidden is truly a catastrophe!</h5>
 {/* <h4 style={{margin:"50px"}}>Almost 80% of the world's populations cannot see the Milky Way band, and such beauty being hidden is truly a catastrophe!</h4> */}
 
