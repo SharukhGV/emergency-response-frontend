7a70f384bc33e677769733a4d1d6c9dd888322f1
sharukh.g.velupillai@gmail.com <sharukh.g.velupillai@gmail.com>
2024-05-15 14:06:30 -0400

    added routes newitem, all items, some buttons

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import Logout from "./Logout";
 // import Menu from "../Menu";
-import hivelogoClear from "./hivelogoClear.png"
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -9,37 +8,12 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import telescopeHome from "./telescopeHome.png"
 function NavigationBar({loginUsername, toggleLOGIN,settoggleLOGIN, accessToken}) {
-//     return(
-//   <div style={{backgroundColor:"black", paddingBottom:"15px",paddingTop:"10px"}}>
-//     {/* <span style={{backgroundColor:"black", paddingTop:"10px", paddingRight:"100%"}}></span> */}
 
-//       <span>{!!toggleLOGIN ? <Logout toggleLOGIN={toggleLOGIN} settoggleLOGIN={settoggleLOGIN} loginUsername={loginUsername}/> :null}</span>
-
-//   {!!toggleLOGIN ? <Menu />:null}
- 
-//   {!toggleLOGIN ?<Link to="/">🏡</Link>:null}
-//   {!toggleLOGIN ? <span style={{paddingLeft:"7px",paddingRight:"7px"}}></span>:null}
-
-//      {!toggleLOGIN ? <span>
-//    <Link to="/signup">📃</Link>
-//     </span>:null}
-//     {!toggleLOGIN ?<span style={{paddingLeft:"7px",paddingRight:"7px"}}></span>:null}
-//    {!toggleLOGIN ?  <span>
-//      <Link to="/login">💻</Link>
-//     </span>:null}
-
-//   </div>)
-// }
 return (
   <Navbar expand="lg" className="navigationBAr" >
     <Container fluid>
       <Navbar.Brand ><Link to="/"><img style={{width:"50px", height:"50px"}} src={telescopeHome}></img></Link></Navbar.Brand>
-      {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav"> */}
-        {/* <Nav className="me-auto"> */}
-          {/* <Nav.Link ><Link to="/">🏡 Home</Link></Nav.Link> */}
-         
-
+ 
 
           {!!toggleLOGIN ?  <NavDropdown style={{color:"black"}} title="Profile" id="basic-nav-dropdown">
             <NavDropdown.Item ><Link to="/profile">👨🏼‍🚀 Profile</Link> </NavDropdown.Item>
@@ -47,15 +21,22 @@ return (
     
             <NavDropdown.Item ><Logout toggleLOGIN={toggleLOGIN} settoggleLOGIN={settoggleLOGIN} loginUsername={loginUsername}/></NavDropdown.Item>
 
-            {/* <NavDropdown.Divider />
-            <NavDropdown.Item>
-              Separated link
-            </NavDropdown.Item> */}
+       
           </NavDropdown> :null}
 
 
           {!!toggleLOGIN ? <Nav.Link><Link to="/meteorshowers">☄️</Link></Nav.Link>:null}
-          {!!toggleLOGIN ?     <Nav.Link>< Link to="/marketplace">🛍️</Link></Nav.Link> :null}
+          {/* MARKET PALCE DROPDOWN */}
+          {!!toggleLOGIN ?  <NavDropdown style={{color:"black"}} title="🛍️" id="basic-nav-dropdown">
+            <NavDropdown.Item ><Link to="/marketplace">Market Home</Link> </NavDropdown.Item>
+
+    
+            <NavDropdown.Item ><Link to="/marketplace/newitem">New Item</Link></NavDropdown.Item>
+            <NavDropdown.Item ><Link to="/marketplace/index">All Items</Link></NavDropdown.Item>
+
+       
+          </NavDropdown> :null}
+          {/* {!!toggleLOGIN ?     <Nav.Link>< Link to="/marketplace">🛍️</Link></Nav.Link> :null} */}
 
      {!!toggleLOGIN ?     <Nav.Link>< Link to="/visibleplanets">🪐</Link></Nav.Link> :null}
  {!!toggleLOGIN ?  <Nav.Link ><Link to="/index">🌍</Link></Nav.Link>:null}
@@ -63,8 +44,7 @@ return (
           {!toggleLOGIN ?<Nav.Link><Link to="/signup">📃 <strong style={{color:"black"}}>Register</strong></Link></Nav.Link>:null}
 
 
-        {/* </Nav> */}
-      {/* </Navbar.Collapse> */}
+        
     </Container>
   </Navbar>
 );
---
+++ src/components/marketplace/AllProducts.jsx
@@ -0,0 +1,12 @@
+
+
+function AllProducts(){
+
+    return(
+        <div>All Items Shown HERE</div>
+    )
+
+
+}
+
+export default AllProducts
