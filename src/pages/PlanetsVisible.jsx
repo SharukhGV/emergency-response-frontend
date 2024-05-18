157a148f5f5bb4ddec5c594286908e1677fda04d
sharukh.g.velupillai@gmail.com <sharukh.g.velupillai@gmail.com>
2024-05-18 19:54:01 -0400

    added fetch for GET and loading and error handling

--- src/pages/PlanetsVisible.jsx
+++ src/pages/PlanetsVisible.jsx
@@ -4,62 +4,117 @@ import { useEffect, useState } from "react"
 import Planet from "./Planet"
 function PlanetsVisible() {
 
-    const [planets, setPlanets] = useState([])
-    const [latitude, setLatitude] = useState(null);
-    const [longitude, setLongitude] = useState(null);
+  const [planets, setPlanets] = useState([])
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [errorMsg, setErrorMsg] = useState("")
+  const [loading, setLoading] = useState(false)
 
-    useEffect(() => {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-            (position) => {
-              // const latitude = position.coords.latitude;
-              // const longitude = position.coords.longitude;
-              // setLocation({ latitude, longitude });
-              // setLocationFound(true);
-              setLatitude( parseFloat(position.coords.latitude));
-              setLongitude( parseFloat(position.coords.longitude));
-            },
-            (error) => {
-              console.error("Error getting location:", error);
-              // setLocationFound(false);
-              window.alert("Please Enable Location and Refresh This Page");
-            }
-          );
-        } else {
-          console.error("Geolocation is not supported by this browser.");
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // const latitude = position.coords.latitude;
+          // const longitude = position.coords.longitude;
+          // setLocation({ latitude, longitude });
+          // setLocationFound(true);
+          setLatitude(parseFloat(position.coords.latitude));
+          setLongitude(parseFloat(position.coords.longitude));
+          setErrorMsg("")
+        },
+        (error) => {
+          console.error("Error getting location:", error);
           // setLocationFound(false);
+          setErrorMsg(error.message)
+          console.log(error.message)
+          // window.alert("Please Enable Location and Refresh This Page");
         }
-      }, []); // Empty dependency array to run only once
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+      // setLocationFound(false);
+
+    }
+  }, []); // Empty dependency array to run only once
 
 
 
-    useEffect(() => {
-        axios.get(`https://api.visibleplanets.dev/v3?latitude=${Math.round(latitude)}&longitude=${Math.round(longitude)}`).then(res => {
-            // console.log(res.data.data)
-            setPlanets(res.data.data)
+  // useEffect(() => {
+  //     axios.get(`https://api.visibleplanets.dev/v3?latitude=${Math.round(latitude)}&longitude=${Math.round(longitude)}`).then(res => {
+  //         // console.log(res.data.data)
+  //         setPlanets(res.data.data)
+  //     }
+  //     )
+
+  // }, [latitude, longitude])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        // setErrorMsg("");
+        setLoading(true);
+        const response = await fetch(`https://api.visibleplanets.dev/v3?latitude=${Math.round(latitude)}&longitude=${Math.round(longitude)}`);
+        if (response.ok) {
+          // handle success
+          const { data } = await response.json();
+          setPlanets(data)
+        } else {
+          // 400, 404, 500, etc. will end up here
+          // handle error response from server
+          const { error } = await response.json();
+          setErrorMsg(error.message);
         }
-        )
+      } catch (err) {
+        setErrorMsg(err.message);
+      } finally {
+        // whatever goes in finally is guaranteed to run
+        // even if we go into catch
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [latitude, longitude]);
 
-    }, [latitude, longitude])
+  // console.log(planets)
 
-// console.log(planets)
+  function renderPlanets() {
+    if (loading) return (
+      <div id="loading-wrapper">
+        <div id="loading-text">LOADING</div>
+        <div id="loading-content"></div>
+      </div>
 
-
-    return (
-
-
+    )
+    else if (errorMsg !== "") {
+     return <div>
+      <br></br>
+     <h1 style={{color:"red"}}> OOPS! {errorMsg}</h1>
+      
+      </div>
+    }
+    else {
+      return (
         <div>
 
-<h1 style={{color:"purple"}}>Current Visible Planets</h1>
-<h3 style={{color:"yellowgreen"}}>Latitude: {latitude}</h3>
-<h3 style={{color:"yellowgreen"}}>Longitude: {longitude}</h3>
-             {planets.map((planet, index)=>{
-            return(
-            <Planet key={index} planet={planet} />
+          <h1 style={{ color: "blue" }}>Current Visible Planets</h1>
+          <h3 style={{ color: "gray" }}>Latitude: {latitude}</h3>
+          <h3 style={{ color: "gray" }}>Longitude: {longitude}</h3>
+          {planets.map((planet, index) => {
+            return (
+              <Planet key={index} planet={planet} />
             )
-            })} 
+          })}
         </div>
-    )
+      )
+    }
+
+  }
+
+  return (
+
+    <>{renderPlanets()}</>
+
+  )
 
 
 
