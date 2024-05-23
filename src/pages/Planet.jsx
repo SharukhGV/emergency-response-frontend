449be9e74d2faae3a64f8e9c5dcef68187ddc180
sharukh.g.velupillai@gmail.com <sharukh.g.velupillai@gmail.com>
2024-05-23 02:55:05 -0400

    added the moon

--- src/pages/Planet.jsx
+++ src/pages/Planet.jsx
@@ -5,185 +5,215 @@ import jupiter from "./jupiter.png"
 import saturn from "./saturn.png"
 import uranus from "./uranus.png"
 import neptune from "./neptune.png"
+import MoonTonight from "./MoonTonight.png"
+
 // import pluto from "./pluto.png"
 // import sun from "./sun.png"
 import lagoon from "./lagoon.jpg"
-function Planet({planet}){
-console.log(planet)
+function Planet({ planet }) {
+  console.log(planet)
 
 
 
 
-return(
+  return (
 
-<div>
-{/* <div>{planet.name === "Sun" ? <img class="planetimages" src={sun}></img> : null}</div> */}
+    <div>
+      {/* <div>{planet.name === "Sun" ? <img class="planetimages" src={sun}></img> : null}</div> */}
+      <>{(planet.name === "Moon" && planet.aboveHorizon === true) ?
 
-<div>{planet.name === "Mercury" ? 
 
-<div style={{fontFamily:"Arial"}} className="card">
-      <img src={lagoon} className="card__image" alt="" />
-      <div className="card__overlay">
-        <div className="card__header">
-          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <img className="card__thumb" src={mercury} alt="" />
-          <div className="card__header-text">
-            <h5 style={{fontFamily:"Arial"}} >{planet.name}</h5>
-            <h3 className="card__title">{planet.constellation}</h3>            
-            <span style={{fontFamily:"Arial"}}  className="card__status">Magnitude: {planet.magnitude}</span>
+        <>
+          <div style={{ fontFamily: "Arial" }} className="card">
+            <img src={lagoon} className="card__image" alt="" />
+            <div className="card__overlay">
+              <div className="card__header">
+                <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                <img className="card__thumb" src={MoonTonight} alt="" />
+                <div className="card__header-text">
+                  <h5 style={{ fontFamily: "Arial" }} >{planet.name}</h5>
+                  <h3 className="card__title">{planet.constellation}</h3>
+                  <span style={{ fontFamily: "Arial" }} className="card__status">Magnitude: {planet.magnitude}</span>
+                </div>
+              </div>
+              {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
+              <p>Need a Telescope? {!planet.nakedEyeObject ? "Yes" : "No"}</p>
+              <p>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>
+
+              <div></div>
+
+            </div>
+          </div>   </>
+
+
+        : null}</>
+
+      <div></div>
+      <div></div><div>{planet.name === "Mercury" ?
+
+        <div style={{ fontFamily: "Arial" }} className="card">
+          <img src={lagoon} className="card__image" alt="" />
+          <div className="card__overlay">
+            <div className="card__header">
+              <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+              <img className="card__thumb" src={mercury} alt="" />
+              <div className="card__header-text">
+                <h5 style={{ fontFamily: "Arial" }} >{planet.name}</h5>
+                <h3 className="card__title">{planet.constellation}</h3>
+                <span style={{ fontFamily: "Arial" }} className="card__status">Magnitude: {planet.magnitude}</span>
+              </div>
+            </div>
+            {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
+            <p>Need a Telescope? {!planet.nakedEyeObject ? "Yes" : "No"}</p>
+            <p>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>
+
+            <div></div>
+
           </div>
         </div>
-        {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
-<p>Need a Telescope? {!planet.nakedEyeObject? "Yes" : "No"}</p>
-<p>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>       
+        : null}</div>
 
-        <div></div>
+      <div></div>
+      <div></div>
+      <div>{planet.name === "Venus" ?
 
-      </div>
-    </div>    
- : null}</div>
+        <div style={{ fontFamily: "Arial" }} className="card">
+          <img src={lagoon} className="card__image" alt="" />
+          <div className="card__overlay">
+            <div className="card__header">
+              <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+              <img className="card__thumb" src={venus} alt="" />
+              <div className="card__header-text">
+                <h5 style={{ fontFamily: "Arial" }} >{planet.name}</h5>
+                <h3 className="card__title">{planet.constellation}</h3>
+                <span style={{ fontFamily: "Arial" }} className="card__status">Magnitude: {planet.magnitude}</span>
+              </div>
+            </div>
+            {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
+            <p>Need a Telescope? {!planet.nakedEyeObject ? "Yes" : "No"}</p>
+            <p>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>
 
-<div></div>
-<div></div>
-<div>{planet.name === "Venus" ? 
+            <div></div>
 
-<div style={{fontFamily:"Arial"}} className="card">
-      <img src={lagoon} className="card__image" alt="" />
-      <div className="card__overlay">
-        <div className="card__header">
-          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <img className="card__thumb" src={venus} alt="" />
-          <div className="card__header-text">
-            <h5 style={{fontFamily:"Arial"}} >{planet.name}</h5>
-            <h3 className="card__title">{planet.constellation}</h3>            
-            <span style={{fontFamily:"Arial"}}  className="card__status">Magnitude: {planet.magnitude}</span>
           </div>
         </div>
-        {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
-<p>Need a Telescope? {!planet.nakedEyeObject? "Yes" : "No"}</p>
-<p>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>       
+        : null}</div>
+      <div>{planet.name === "Mars" ?
 
-        <div></div>
+        <div style={{ fontFamily: "Arial" }} className="card">
+          <img src={lagoon} className="card__image" alt="" />
+          <div className="card__overlay">
+            <div className="card__header">
+              <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+              <img className="card__thumb" src={mars} alt="" />
+              <div className="card__header-text">
+                <h5 style={{ fontFamily: "Arial" }} >{planet.name}</h5>
+                <h3 className="card__title">{planet.constellation}</h3>
+                <span style={{ fontFamily: "Arial" }} className="card__status">Magnitude: {planet.magnitude}</span>
+              </div>
+            </div>
+            {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
+            <p>Need a Telescope? {!planet.nakedEyeObject ? "Yes" : "No"}</p>
+            <p>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>
 
-      </div>
-    </div>    
- : null}</div>
-<div>{planet.name === "Mars" ? 
+            <div></div>
 
-<div style={{fontFamily:"Arial"}} className="card">
-      <img src={lagoon} className="card__image" alt="" />
-      <div className="card__overlay">
-        <div className="card__header">
-          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <img className="card__thumb" src={mars} alt="" />
-          <div className="card__header-text">
-            <h5 style={{fontFamily:"Arial"}} >{planet.name}</h5>
-            <h3 className="card__title">{planet.constellation}</h3>            
-            <span style={{fontFamily:"Arial"}}  className="card__status">Magnitude: {planet.magnitude}</span>
           </div>
         </div>
-        {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
-<p>Need a Telescope? {!planet.nakedEyeObject? "Yes" : "No"}</p>
-<p>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>       
+        : null}</div>
+      <div>{planet.name === "Jupiter" ?
 
-        <div></div>
+        <div style={{ fontFamily: "Arial" }} className="card">
+          <img src={lagoon} className="card__image" alt="" />
+          <div className="card__overlay">
+            <div className="card__header">
+              <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+              <img className="card__thumb" src={jupiter} alt="" />
+              <div className="card__header-text">
+                <h5 style={{ fontFamily: "Arial" }} >{planet.name}</h5>
+                <h3 className="card__title">{planet.constellation}</h3>
+                <span style={{ fontFamily: "Arial" }} className="card__status">Magnitude: {planet.magnitude}</span>
+              </div>
+            </div>
+            {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
+            <p style={{ color: "black" }}>Need a Telescope? {!planet.nakedEyeObject ? "Yes" : "No"}</p>
+            <p style={{ color: "black" }}>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>
 
-      </div>
-    </div>    
- : null}</div>
-<div>{planet.name === "Jupiter" ? 
+            <div></div>
 
-<div style={{fontFamily:"Arial"}} className="card">
-      <img src={lagoon} className="card__image" alt="" />
-      <div className="card__overlay">
-        <div className="card__header">
-          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <img className="card__thumb" src={jupiter} alt="" />
-          <div className="card__header-text">
-            <h5 style={{fontFamily:"Arial"}} >{planet.name}</h5>
-            <h3 className="card__title">{planet.constellation}</h3>            
-            <span style={{fontFamily:"Arial"}}  className="card__status">Magnitude: {planet.magnitude}</span>
           </div>
         </div>
-        {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
-<p style={{color:"black"}}>Need a Telescope? {!planet.nakedEyeObject? "Yes" : "No"}</p>
-<p style={{color:"black"}}>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>       
+        : null}</div>
+      <div>{planet.name === "Saturn" ?
 
-        <div></div>
+        <div style={{ fontFamily: "Arial" }} className="card">
+          <img src={lagoon} className="card__image" alt="" />
+          <div className="card__overlay">
+            <div className="card__header">
+              <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+              <img className="card__thumb" src={saturn} alt="" />
+              <div className="card__header-text">
+                <h5 style={{ fontFamily: "Arial" }} >{planet.name}</h5>
+                <h3 className="card__title">{planet.constellation}</h3>
+                <span style={{ fontFamily: "Arial" }} className="card__status">Magnitude: {planet.magnitude}</span>
+              </div>
+            </div>
+            {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
+            <p>Need a Telescope? {!planet.nakedEyeObject ? "Yes" : "No"}</p>
+            <p>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>
 
-      </div>
-    </div>    
- : null}</div>
-<div>{planet.name === "Saturn" ? 
+            <div></div>
 
-<div style={{fontFamily:"Arial"}} className="card">
-      <img src={lagoon} className="card__image" alt="" />
-      <div className="card__overlay">
-        <div className="card__header">
-          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <img className="card__thumb" src={saturn} alt="" />
-          <div className="card__header-text">
-            <h5 style={{fontFamily:"Arial"}} >{planet.name}</h5>
-            <h3 className="card__title">{planet.constellation}</h3>            
-            <span style={{fontFamily:"Arial"}}  className="card__status">Magnitude: {planet.magnitude}</span>
           </div>
         </div>
-        {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
-<p>Need a Telescope? {!planet.nakedEyeObject? "Yes" : "No"}</p>
-<p>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>       
+        : null}</div>
+      <div>{planet.name === "Uranus" ?
 
-        <div></div>
+        <div style={{ fontFamily: "Arial" }} className="card">
+          <img src={lagoon} className="card__image" alt="" />
+          <div className="card__overlay">
+            <div className="card__header">
+              <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+              <img className="card__thumb" src={uranus} alt="" />
+              <div className="card__header-text">
+                <h5 style={{ fontFamily: "Arial" }} >{planet.name}</h5>
+                <h3 className="card__title">{planet.constellation}</h3>
+                <span style={{ fontFamily: "Arial" }} className="card__status">Magnitude: {planet.magnitude}</span>
+              </div>
+            </div>
+            {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
+            <p>Need a Telescope? {!planet.nakedEyeObject ? "Yes" : "No"}</p>
+            <p>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>
 
-      </div>
-    </div>    
- : null}</div>
-<div>{planet.name === "Uranus" ? 
+            <div></div>
 
-<div style={{fontFamily:"Arial"}} className="card">
-      <img src={lagoon} className="card__image" alt="" />
-      <div className="card__overlay">
-        <div className="card__header">
-          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <img className="card__thumb" src={uranus} alt="" />
-          <div className="card__header-text">
-            <h5 style={{fontFamily:"Arial"}} >{planet.name}</h5>
-            <h3 className="card__title">{planet.constellation}</h3>            
-            <span style={{fontFamily:"Arial"}}  className="card__status">Magnitude: {planet.magnitude}</span>
           </div>
         </div>
-        {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
-<p>Need a Telescope? {!planet.nakedEyeObject? "Yes" : "No"}</p>
-<p>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>       
+        : null}</div>
+      <div>{planet.name === "Neptune" ?
 
-        <div></div>
+        <div style={{ fontFamily: "Arial" }} className="card">
+          <img src={lagoon} className="card__image" alt="" />
+          <div className="card__overlay">
+            <div className="card__header">
+              <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+              <img className="card__thumb" src={neptune} alt="" />
+              <div className="card__header-text">
+                <h5 style={{ fontFamily: "Arial" }} >{planet.name}</h5>
+                <h3 className="card__title">{planet.constellation}</h3>
+                <span style={{ fontFamily: "Arial" }} className="card__status">Magnitude: {planet.magnitude}</span>
+              </div>
+            </div>
+            {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
+            <p>Need a Telescope? {!planet.nakedEyeObject ? "Yes" : "No"}</p>
+            <p>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>
 
-      </div>
-    </div>    
-: null}</div>
-<div>{planet.name === "Neptune" ? 
+            <div></div>
 
-<div style={{fontFamily:"Arial"}} className="card">
-      <img src={lagoon} className="card__image" alt="" />
-      <div className="card__overlay">
-        <div className="card__header">
-          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <img className="card__thumb" src={neptune} alt="" />
-          <div className="card__header-text">
-            <h5 style={{fontFamily:"Arial"}} >{planet.name}</h5>
-            <h3 className="card__title">{planet.constellation}</h3>            
-            <span style={{fontFamily:"Arial"}}  className="card__status">Magnitude: {planet.magnitude}</span>
           </div>
         </div>
-        {/* <p style={{fontFamily:"Arial", fontSize:"10px"}}  className="card__description">{person.description}</p> */}
-<p>Need a Telescope? {!planet.nakedEyeObject? "Yes" : "No"}</p>
-<p>Above Horizon? {planet.aboveHorizon ? "Yes" : "No"}</p>       
-
-        <div></div>
-
-      </div>
-    </div>    
- : null}</div>
-{/* <div>{planet.name === "Pluto" ? <img class="planetimages" src={pluto}></img> : null}</div> */}
+        : null}</div>
+      {/* <div>{planet.name === "Pluto" ? <img class="planetimages" src={pluto}></img> : null}</div> */}
 
 
 
@@ -192,9 +222,9 @@ return(
 
 
 
-</div>
+    </div>
 
-)
+  )
 
 
 }
